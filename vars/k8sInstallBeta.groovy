3effda0d5b534f632467216aad427fa4e4518eff
85matthew <85matthew@gmail.com>
2019-08-05 20:07:10 -0600

    add install function

---
+++ vars/k8sInstallBeta.groovy
@@ -0,0 +1,12 @@
+def call(project, domain, extraValues = "") {
+    chartName = "${project}-${env.BUILD_NUMBER}-${env.BRANCH_NAME}"
+    tagBeta = "${currentBuild.displayName}-${env.BRANCH_NAME}"
+    addr = "${project}-${env.BUILD_NUMBER}-${env.BRANCH_NAME}.${domain}"
+    sh """helm install \
+        helm/${project} \
+        -n ${chartName.toLowerCase()} -i \
+        --tiller-namespace ${project}-build \
+        --set image.tag=${tagBeta} \
+        --set ingress.host=${addr.toLowerCase()} \
+        ${extraValues}"""
+}
